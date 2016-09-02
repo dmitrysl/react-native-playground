4279fcef1b886312441594ea3dc3897619245000
Dmitry Slepchenko <slepchenko.dima@gmail.com>
2016-09-02 12:42:12 +0300

    udpated travis ci config

--- mobile/build-mobile.sh
+++ mobile/build-mobile.sh
@@ -6,3 +6,4 @@ echo $(basename pwd)
 npm install
 react-native bundle --platform android --dev false --entry-file index.android.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
 cd android && ./gradlew assembleRelease
+cd ..
